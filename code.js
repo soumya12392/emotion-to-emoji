e977e27156efd405b1f12aafa5b468fc13c02773
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2021-12-29 11:46:21 +0530

    Add files via upload
    Completed making the "Emotion to Emoji" webapp

--- code.js
+++ code.js
@@ -30,3 +30,49 @@ function speak() {
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+
+function check() {
+    var img = document.getElementById("captured_image");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name1").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if (prediction_1 == "Happy") {
+            document.getElementById("update_emoji1").innerHTML = "&#128522;";
+        }
+
+        if (prediction_1 == "Sad") {
+            document.getElementById("update_emoji1").innerHTML = "&#128532;";
+            document.getElementById("motivational_quote1").innerHTML = "Don't be sad! Because God sends hope in the most desperate moments. Don't forget, the heaviest rain comes out of the darkest clouds.";
+        }
+
+        if (prediction_1 == "Angry") {
+            document.getElementById("update_emoji1").innerHTML = "&#128548;";
+        }
+
+        if (prediction_2 == "Happy") {
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+
+        if (prediction_2 == "Sad") {
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";
+            document.getElementById("motivational_quote2").innerHTML = "Don't be sad! Because God sends hope in the most desperate moments. Don't forget, the heaviest rain comes out of the darkest clouds.";
+        }
+
+        if (prediction_2 == "Angry") {
+            document.getElementById("update_emoji2").innerHTML = "&#128548;";
+        }
+    }
+}
